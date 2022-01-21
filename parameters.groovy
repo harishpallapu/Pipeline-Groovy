7d58a0b044e5ef828814c3db63e16a4533767302
infotechbalu <infotechdemoacc10@gmail.com>
2022-01-21 14:54:46 +0530

    Added Groovy File Parameters

--- parameters.groovy
+++ parameters.groovy
@@ -1,6 +1,6 @@
 env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
-env.TOMCAT_IP=
+env.TOMCAT_IP="11111111111111"
 env.FUNCTIONAL_WEB_GIT_URL="https://github.com/harishpallapu/webtest.git"
 env.FUNCTIONAL_MOBILE_GIT_URL="https://github.com/harishpallapu/mobiletest.git"
 env.JMETER=
